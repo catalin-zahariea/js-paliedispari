25ef9901106b5054c87a12e8a000b31e9c9c9fa5
CATALIN ZAHARIEA <zahariea.c@gmail.com>
2021-03-27 09:32:23 +0100

    Tried to make the checkPalindrom function more efficient.

--- Parola Palindroma/js/script.js
+++ Parola Palindroma/js/script.js
@@ -5,9 +5,13 @@ var userInputString = prompt('Inserica una parola');
 console.log(`La parola inserita è la seguente: ${userInputString}`);
 
 // ----------------------------------------
-// Function check
+// Function print
 
-console.log(checkPalindrome(userInputString));
+if ( checkPalindrome(userInputString) == true ) {
+    alert ('La parola inserita è palindroma!');
+} else {
+    alert('La parola inserita non è palindroma!');
+}
 
 // ----------------------------------------
 // Function
@@ -17,29 +21,49 @@ function checkPalindrome(string) {
     // ----------------------------------------
     // Variables   
 
-    var result;   
-    var flagStop = true;
+    var result = true;
+    var i = 0;
 
     // ----------------------------------------
+    // FOR Loop that goes through half of the string. Basically I check the first half is == to the second half.
 
-    for ( var i = 0; i < string.length / 2 && flagStop; i++) {
+    // for ( var i = 0; i < string.length / 2; i++) {
 
+    //     // ----------------------------------------
+    //     // String characters check
+    //     console.log(string.charAt(i));
+    //     console.log(string.charAt(string.length - i - 1));
+
+    //     // ----------------------------------------
+    //     // .charAt() function returns the character at the specified index inside the parenthesis
+        
+    //     if ( string.charAt(i) == string.charAt(string.length - i - 1) ) {
+    //         result = true;
+    //     }
+    // }
+
+
+
+    // ----------------------------------------
+    // DO WHILE Loop that goes through half the string. Initial result variable value changed to TRUE so that the loop can run. 
+    // It will stop as soon as it find 2 different characters in a mirror position.
+
+    do {
         // ----------------------------------------
         // String characters check
-
         console.log(string.charAt(i));
         console.log(string.charAt(string.length - i - 1));
 
         // ----------------------------------------
         // .charAt() function returns the character at the specified index inside the parenthesis
         
-        if ( string.charAt(i) == string.charAt(string.length - i - 1) ) {
-            result ='La parola è palindroma!';
-        } else {
-            result ='La parola non è palindroma!';
-            flagStop = false;
+        if ( string.charAt(i) != string.charAt(string.length - i - 1) ) {
+            result = false;
         }
-    }
+
+        i++;
+
+    } while (i < string.length / 2 && result == true); 
 
     return result;
 }
